547ed9e68453d2b88bfedfdda85457ed5e659709
Ptolemy <ptolemybarnes@gmail.com>
2015-05-26 09:57:22 +0100

    Refactor styling of test descriptions

--- spec/JavaBuzzSpec.js
+++ spec/JavaBuzzSpec.js
@@ -2,22 +2,25 @@ describe('JavaBuzz', function() {
   var javabuzz;
 
   describe('#call', function() {
-    javabuzz = new JavaBuzz();
+    beforeEach(function() {
+      javabuzz = new JavaBuzz();
+    });
     
-    it('returns Fizz when passed number divisible by 3', function() {
+    it("returns 'Fizz' when passed number divisible by 3", function() {
       expect(javabuzz.call(3)).toEqual('Fizz'); 
     });
 
-    it('returns Buzz when passed number divisible by 5', function() {
+    it("returns 'Buzz' when passed number divisible by 5", function() {
       expect(javabuzz.call(5)).toEqual('Buzz');
     });
 
-    it('returns FizzBuzz when passed number divisible by 15', function() {
+    it("returns 'FizzBuzz' when passed number divisible by 15", function() {
       expect(javabuzz.call(15)).toEqual('FizzBuzz');
     });
 
-    it('returns the number when it is not divisible by 3, 5, or 15', function() {
+    it("returns number when it is not divisible by 3, 5, or 15", function() {
       expect(javabuzz.call(19)).toEqual(19);
     });
   });
 });
+
