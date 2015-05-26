ae16e4309f8568c2505b13c50d775bd218fad3c2
Ptolemy <ptolemybarnes@gmail.com>
2015-05-26 09:50:41 +0100

    Passed unit test: JavaBuzz returns 'Buzz', 'FizzBuzz' and number under correct conditions

--- spec/JavaBuzzSpec.js
+++ spec/JavaBuzzSpec.js
@@ -2,10 +2,22 @@ describe('JavaBuzz', function() {
   var javabuzz;
 
   describe('#call', function() {
-
+    javabuzz = new JavaBuzz();
+    
     it('returns Fizz when passed number divisible by 3', function() {
-      javabuzz = new JavaBuzz();
       expect(javabuzz.call(3)).toEqual('Fizz'); 
+    });
+
+    it('returns Buzz when passed number divisible by 5', function() {
+      expect(javabuzz.call(5)).toEqual('Buzz');
+    });
+
+    it('returns FizzBuzz when passed number divisible by 15', function() {
+      expect(javabuzz.call(15)).toEqual('FizzBuzz');
+    });
+
+    it('returns the number when it is not divisible by 3, 5, or 15', function() {
+      expect(javabuzz.call(19)).toEqual(19);
     });
   });
 });
--- src/JavaBuzz.js
+++ src/JavaBuzz.js
@@ -1,7 +1,16 @@
 var JavaBuzz = function() {}
 
 JavaBuzz.prototype.call = function(number) {
-  if (number % 3 == 0) {
+  if (number % 15 == 0) {
+    return 'FizzBuzz'
+  } 
+  else if (number % 5 == 0) {
+    return 'Buzz'
+  }
+  else if (number % 3 == 0) {
     return 'Fizz'
+  }
+  else {
+    return number;
   }
 }
